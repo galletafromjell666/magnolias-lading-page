893a35270492a8bac02ecbfd4a8616759139e894
galletafromjell666 <giococu666@gmail.com>
2022-12-05 22:27:01 -0600

    fix responsive layout

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -2,9 +2,9 @@ import React from "react";
 
 const HeroSection = () => {
   return (
-    <div className="w-full h-[35rem] flex flex-col md:flex-row bg-primary ">
-      <div className="flex-1 w-40 ">
-        <div className="flex h-full w-full justify-center items-center flex-col text-6xl">
+    <div className="w-full min-w-fit bg-primary flex flex-col md:flex-row md:h-[35rem] ">
+      <div className="flex-1 w-100 md:w-40 ">
+        <div className="flex h-full w-full pt-10 justify-center items-center flex-col text-6xl md:pt-0">
           <h1>
           Professional<br></br>
             spa and beauty <br></br>center
@@ -12,7 +12,7 @@ const HeroSection = () => {
           <h2 className=" mt-8 text-2xl">Are u a girl looking for some beauty?</h2>
         </div>
       </div>
-      <div className="flex-1 w-60 relative">
+      <div className="relative flex-1 w-100 md:w-60 ">
         <img
           className="h-full mx-auto"
           src="/hero-pexels-vincenzo-giove-1925482.png"
