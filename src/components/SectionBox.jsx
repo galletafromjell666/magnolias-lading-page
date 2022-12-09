dc868109aed6931a8bd778eb5382fb8afa197acb
galletafromjell666 <giococu666@gmail.com>
2022-12-08 22:31:16 -0600

    fix mobile grid

--- src/components/SectionBox.jsx
+++ src/components/SectionBox.jsx
@@ -3,8 +3,8 @@ import React from "react";
 const SectionBox = ({title,imgSrc,tailwindClass}) => {
  // console.log(title,imgSrc,tailwindClass)
   return (
-    <div className={tailwindClass}>
-      <img style={{"objectFit": "cover"}} className="w-full h-[calc(100%-3rem)] rounded-lg" src={imgSrc} alt={title}/>
+    <div id="uwu" className={tailwindClass }>
+      <img style={{"objectFit": "cover"}} className=" w-full h-[calc(100%-3rem)] rounded-lg" src={imgSrc} alt={title}/>
       <h1 className="text-3xl font-light text-black/75 h-[3rem] ">{title}</h1>
     </div>
   );
--- src/components/ServicesSection.jsx
+++ src/components/ServicesSection.jsx
@@ -8,63 +8,62 @@ const ServicesSection = () => {
       title: "Mechas",
       imgSrc:
         "https://images.pexels.com/photos/3065207/pexels-photo-3065207.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-      tailwindClass: "row-span-1",
+      tailwindClass: "row-span-1 ",
     },
     {
       id: 2,
       title: "Cuidados",
       imgSrc:
         "https://images.pexels.com/photos/3993461/pexels-photo-3993461.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-      tailwindClass: "row-span-2",
+      tailwindClass: "md:row-span-2",
     },
     {
       id: 3,
       title: "Alisado",
       imgSrc:
         "https://images.pexels.com/photos/3738359/pexels-photo-3738359.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-      tailwindClass: "",
+      tailwindClass: "col-span-2 md:col-span-1",
     },
     {
       id: 4,
       title: "Balayage",
       imgSrc:
         "https://images.pexels.com/photos/1321916/pexels-photo-1321916.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-      tailwindClass: "row-span-2",
+      tailwindClass: "md:row-span-2",
     },
     {
       id: 5,
       title: "Cortes",
       imgSrc:
         "https://images.pexels.com/photos/11595337/pexels-photo-11595337.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-      tailwindClass: "row-span-2",
+      tailwindClass: "md:row-span-2",
     },
     {
       id: 6,
       title: "Acrilismo",
       imgSrc:
         "https://images.pexels.com/photos/3997379/pexels-photo-3997379.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-      tailwindClass: "",
+      tailwindClass: "col-span-2 md:col-span-1",
     },
   ];
   //console.log(test01);
   return (
-    <div className="w-full mx-auto bg-primary">
-      <div className=" md:px-[70px] w-full  bg-primary h-[auto]">
-      <div className="w-full pl-8 py-8">
-        <h1 className="text-3xl font-semibold">Browse Services</h1>
-        <h2 className="text-xl  font-light text-black/75">
-          Manage our bussiness
-        </h2>
-      </div>
-      <div className="grid grid-cols-1 pb-10 md:grid-cols-3 grid-rows grid-flow-row sm:gap-6 sm:px-6 px-10 ">
-        {test01.map((uwu) => {
-          //console.log(uwu);
-          return <SectionBox key={uwu.id} {...uwu} />;
-        })}
+    <div className="w-full bg-primary">
+      <div className="mx-auto bg-primary h-[auto] w-[90%]  md:w-[90%] 3xl:container 3xl:w-[90%] xl:px-8">
+        <div className="w-full  py-8">
+          <h1 className="text-3xl font-averia font-semibold">Browse Services</h1>
+          <h2 className="text-xl  font-inter font-light text-black/75">
+            Manage our bussiness
+          </h2>
+        </div>
+        <div className="grid pb-[100px] font-averia grid-cols-2 h-auto w-full grid-rows-custom grid-flow-row gap-6 md:gap-x-10 md:h-[750px] md:grid-cols-3 xl:h-[1100px] xl:gap-x-20 md:gap-y-10 ">
+          {test01.map((uwu) => {
+            //console.log(uwu);
+            return <SectionBox key={uwu.id} {...uwu} />;
+          })}
+        </div>
       </div>
     </div>
-    </div>
-    
   );
 };
 
