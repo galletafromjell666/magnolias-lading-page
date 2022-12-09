0debe8ab8cdd3c77d625aa325514e56164a85120
galletafromjell666 <giococu666@gmail.com>
2022-12-08 22:30:33 -0600

    add new fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,36 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {colors: {
-      primary: "#EDD5D8",
-      white: "#FAF2F2",
-      brick: "#FCF4F0"
-    },},
+    extend: {
+      colors: {
+        primary: "#EDD5D8",
+        white: "#FAF2F2",
+        brick: "#FCF4F0",
+      },
+      screens: {
+        'sm': '640px',
+      // => @media (min-width: 640px) { ... }
+
+      'md': '768px',
+      // => @media (min-width: 768px) { ... }
+
+      'lg': '1024px',
+      // => @media (min-width: 1024px) { ... }
+
+      'xl': '1280px',
+      // => @media (min-width: 1280px) { ... }
+
+      '2xl': '1536px',
+      // => @media (min-width: 1536px) { ... }
+      '3xl': '1920px',
+      '2k': '2048px',
+      },
+      fontFamily: {
+        averia: ['Averia Serif Libre', 'cursive'],
+        inter: ['Inter', "sans-serif"]
+      }
+    },
   },
   plugins: [],
-}
+};
