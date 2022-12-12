350b20749da3a2fbea77cd2de5a1baec8f98aaca
galletafromjell666 <giococu666@gmail.com>
2022-12-11 18:28:23 -0600

    fix navbar z position

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -35,9 +35,6 @@ const Navbar = () => {
             viewBox="0 0 20 20"
             xmlns="http://www.w3.org/2000/svg"
           >
-            <path
-              d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"
-            ></path>
           </svg>
         </button>
         <div
@@ -78,16 +75,13 @@ const Navbar = () => {
                   viewBox="0 0 20 20"
                   xmlns="http://www.w3.org/2000/svg"
                 >
-                  <path
-                    d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
-                  ></path>
                 </svg>
               </button>
               <div
                 id="dropdownNavbar"
                 className={
                   (isDropdownHover ? "absolute" : "hidden") +
-                  " drop-shadow-2xl z-10 font-normal bg-primary divide-y divide-gray-100 rounded shadow w-44 "
+                  " drop-shadow-2xl z-20 font-normal bg-primary divide-y divide-gray-100 rounded shadow w-44 "
                 }
               >
                 <ul
